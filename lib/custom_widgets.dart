190fbd6029b749d0287ebeaaaeef854a2da84822
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2024-08-02 10:50:17 -0600

    Agregar perfil y cards de horas voae a pantalla inicio

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -7,19 +7,53 @@ class BarraInferior extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Inicio",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.schedule),
-            label: "Horas VOAE",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Configuración",
-          ),
-        ],
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Inicio",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.schedule),
+          label: "Horas VOAE",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: "Configuración",
+        ),
+      ],
+    );
+  }
+}
+
+class CardHorasInicio extends StatelessWidget {
+  final String ambito;
+  const CardHorasInicio({required this.ambito, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 40.0, 20.0, 40.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Horas Ambito $ambito',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              '0/10',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
