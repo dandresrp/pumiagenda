8109f200f13d8b5e990675733760a3feb0afb0e9
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2024-08-02 23:45:49 -0600

    Agregar pantalla horas VOAE y paquete go_router

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class BarraInferior extends StatelessWidget {
-  const BarraInferior({super.key});
+class BarraInferior extends StatefulWidget {
+  final int currentIndex;
+  const BarraInferior({required this.currentIndex, super.key});
+
+  @override
+  State<BarraInferior> createState() => _BarraInferiorState();
+}
+
+class _BarraInferiorState extends State<BarraInferior> {
+  late int currentPageIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    currentPageIndex = widget.currentIndex;
+  }
+
+  void _navigateToPage(int index) {
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/');
+        break;
+      case 1:
+        GoRouter.of(context).go('/horasvoae');
+        break;
+      // case 2:
+      //   GoRouter.of(context).go('/configuracion');
+      //   break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "Inicio",
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+          _navigateToPage(index);
+        });
+      },
+      selectedIndex: currentPageIndex,
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          label: 'Inicio',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.schedule),
-          label: "Horas VOAE",
+        NavigationDestination(
+          icon: Icon(Icons.watch_later_outlined),
+          label: 'Horas VOAE',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: "Configuración",
+        NavigationDestination(
+          icon: Icon(Icons.settings_outlined),
+          label: 'Configuración',
         ),
       ],
     );
@@ -33,7 +69,7 @@ class CardHorasInicio extends StatelessWidget {
     return Card(
       elevation: 4,
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(10.0, 40.0, 20.0, 40.0),
+        padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 40.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -51,6 +87,38 @@ class CardHorasInicio extends StatelessWidget {
                 color: Colors.blueAccent,
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardHorasVoae extends StatelessWidget {
+  final String ambito;
+  final Icon icon;
+  const CardHorasVoae({required this.ambito, required this.icon, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 40.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Ambito $ambito',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            CircleAvatar(
+              radius: 30.0,
+              child: icon,
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:pumiagenda/pantallas/pantalla_inicio.dart';
+import 'package:pumiagenda/pantallas/pantalla_horas_voae.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const PantallaInicio(),
+    ),
+    GoRoute(
+      path: '/horasvoae',
+      builder: (context, state) => const PantallaHorasVoae(),
+    ),
+    // GoRoute(
+    //   path: '/configuracion',
+    //   builder: (context, state) => const PantallaConfiguracion(),
+    // )
+  ]
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(),
       themeMode: ThemeMode.system,
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: const Inicio()
+      routerConfig: router,
     );
   }
 }
