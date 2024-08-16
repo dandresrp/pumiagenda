b31cb335c3966008a067dd5bcd951d81a9421fa0
Roberto Sanchez <robertosanchez@unah.hn>
2024-08-16 00:20:37 -0600

    Implementa Validaciones en pantalla_detalles

--- lib/pantallas/pantalla_detalles.dart
+++ lib/pantallas/pantalla_detalles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PantallaDetalles extends StatefulWidget {
   final String extrasData;
@@ -8,6 +9,59 @@ class PantallaDetalles extends StatefulWidget {
 }
 
 class _PantallaDetallesState extends State<PantallaDetalles> {
+  final _formKey = GlobalKey<FormState>();
+  final _nombreActividad = TextEditingController();
+  final _descripcionActividad = TextEditingController();
+  final _fechaActividad = TextEditingController();
+  final _ambitoActividad = TextEditingController();
+
+  @override
+
+  void dispose() {
+    _nombreActividad.dispose();
+    _descripcionActividad.dispose();
+    _fechaActividad.dispose();
+    _ambitoActividad.dispose();
+    super.dispose();
+  }
+
+  //Validar Nombre de Actividades
+  String? _validateNombre(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Valor obligatorio';
+    }
+    if (value.length > 24) {
+      return 'Nombre demasiado largo';
+    }
+    return null;
+  }
+
+  //Validar Descripción de Actividad
+  String? _validateDescripcion(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Sin descripción';
+    }
+    if (value.length > 50) {
+      return 'Descripción demasiado larga';
+    }
+    return null;
+  }
+
+  //Validar Fecha
+  String? _validateFecha(DateTime? value) {
+    if (value == null ) {
+      return 'Valor obligatorio';
+    }
+    return null;
+  }
+
+  //Validar ambito
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      context.go('/detalles'); 
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +69,23 @@ class _PantallaDetallesState extends State<PantallaDetalles> {
     return Scaffold(
       appBar: AppBar(
         title: Text(nombre),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
       ),
       body: const SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Text('')
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('PANTALLA EN MANTENIMIENTO',
+                  style: TextStyle(fontSize: 40),
+                  textAlign: TextAlign.center,
+                ),
+                Icon(Icons.browser_not_supported,size: 100,color: Colors.amber,)              
+              ],
+            ),
+          )
+
         )
       )
     );
