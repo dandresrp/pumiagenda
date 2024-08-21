0272a2bede40e16381df82d9c55e928a9c506e27
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2024-08-20 22:34:52 -0600

    Arreglar conflictos de ultimo commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:pumiagenda/pantallas/pantalla_acercade.dart';
-import 'package:pumiagenda/pantallas/pantalla_crear_actividad.dart';
 import 'firebase_options.dart';
 import 'package:pumiagenda/pantallas/pantalla_navegacion.dart';
 import 'package:pumiagenda/pantallas/pantalla_detalles.dart';
