2f7acd144968e133cdd2ca1fe6a895f23c291877
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2024-08-20 22:50:44 -0600

    Corregir cosas

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:pumiagenda/pantallas/pantalla_acercade.dart';
 import 'firebase_options.dart';
+import 'package:pumiagenda/pantallas/pantalla_acercade.dart';
+import 'pantallas/pantalla_crear_actividad.dart';
 import 'package:pumiagenda/pantallas/pantalla_navegacion.dart';
 import 'package:pumiagenda/pantallas/pantalla_detalles.dart';
 import 'package:pumiagenda/pantallas/pantalla_inicio.dart';
@@ -23,52 +24,58 @@ void main() async {
   runApp(const MyApp());
 }
 
-GoRouter router = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const PantallaNavegacion(),
-  ),
-  GoRoute(
-    path: '/inicio',
-    builder: (context, state) => const PantallaInicio(),
-  ),
-  GoRoute(
-    path: '/registro',
-    builder: (context, state) => const PantallaRegistro(),
-  ),
-  GoRoute(
-      path: '/editarPerfil',
+GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const PantallaNavegacion(),
+    ),
+    GoRoute(
+      path: '/inicio',
+      builder: (context, state) => const PantallaInicio(),
+    ),
+    GoRoute(
+      path: '/registro',
+      builder: (context, state) => const PantallaRegistro(),
+    ),
+    GoRoute(
+        path: '/editarPerfil',
+        builder: (context, state) {
+          final extrasData = state.extra as dynamic;
+          return PantallaEditarPerfil(extrasData: extrasData);
+        }),
+    GoRoute(
+      path: '/horasvoae',
+      builder: (context, state) => const PantallaHorasVoae(),
+    ),
+    GoRoute(
+      path: '/ambito',
       builder: (context, state) {
-        final extrasData = state.extra as dynamic;
-        return PantallaEditarPerfil(extrasData: extrasData);
-      }),
-  GoRoute(
-    path: '/horasvoae',
-    builder: (context, state) => const PantallaHorasVoae(),
-  ),
-  GoRoute(
-    path: '/ambito',
-    builder: (context, state) {
-      final extrasData = state.extra as String;
-      return PantallaAmbito(extrasData: extrasData);
-    },
-  ),
-  GoRoute(
-    path: '/detalles',
-    builder: (context, state) {
-      final extrasData = state.extra as String;
-      return PantallaDetalles(extrasData: extrasData);
-    },
-  ),
-  GoRoute(
-    path: '/configuracion',
-    builder: (context, state) => const PantallaConfiguracion(),
-  ),
-  GoRoute(
-    path: '/acerca',
-    builder: (context, state) => const PantallaAcercade(),
-  )
-]);
+        final extrasData = state.extra as String;
+        return PantallaAmbito(extrasData: extrasData);
+      },
+    ),
+    GoRoute(
+      path: '/detalles',
+      builder: (context, state) {
+        final extrasData = state.extra as String;
+        return PantallaDetalles(extrasData: extrasData);
+      },
+    ),
+    GoRoute(
+      path: '/configuracion',
+      builder: (context, state) => const PantallaConfiguracion(),
+    ),
+    GoRoute(
+      path: '/acerca',
+      builder: (context, state) => const PantallaAcercade(),
+    ),
+    GoRoute(
+      path: '/nuevaActividad',
+      builder: (context, state) => const NuevaActividad(),
+    )
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
